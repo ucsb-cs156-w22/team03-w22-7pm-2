f9567c2c17c8155659bda71565543be4108c6810
AHuangHe <ahuanghe@umail.ucsb.edu>
2022-02-22 02:01:47 -0800

    ah - imported autowired

--- src/main/java/edu/ucsb/cs156/example/controllers/EarthquakesController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/EarthquakesController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import lombok.extern.slf4j.Slf4j;
 
